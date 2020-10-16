a09e60877beef833466cde4effec65ee41605c9e
TraceLosu <kivlanbahmidt@gmail.com>
2020-10-16 23:44:23 +0700

    simplified login and changed variables for clarity

--- GoldenRetriever/GoldenRetriever.py
+++ GoldenRetriever/GoldenRetriever.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 
 LOGIN_URL = 'https://emas.ui.ac.id/login/index.php'
 DASHBOARD_URL = "https://emas.ui.ac.id/my/"
+LOGINERR_CSS = "div.loginerrors > a"
 COURSE_CSS = 'div[id=courses-view-in-progress] > div[id=pc-for-in-progress] > div > div.span6 > div > div > div > div.media-body > h4 > a'
 
 class GoldenRetriever():
@@ -16,25 +17,25 @@ class GoldenRetriever():
         self.courses = CaseInsensitiveDict()
 
     def login(self, username, password):
-        if username and password:
-            payload = {'username': username, 'password': password}
-            login_result = self.session.post(LOGIN_URL, payload)
-            payload = ""
-            return login_result
-        else:
-            return None
+        payload = {'username': username, 'password': password}
+        login_result = self.session.post(LOGIN_URL, payload)
+        login_result = BeautifulSoup(login_result.content, 'html.parser')
+        return True if not login_result.select(LOGINERR_CSS) else False
     
     def get_course(self):
         dashboard = self.session.get(DASHBOARD_URL)
-        dashboard = BeautifulSoup(dashboard, 'html.parser')
-        dashboard = dashboard.select(COURSE_CSS)
-        for course in dashboard:
+        dashboard = BeautifulSoup(dashboard.content, 'html.parser')
+        courses = dashboard.select(COURSE_CSS)
+        for course in courses:
             self.courses[course.text] = course['href']
-        return self.courses
+        return self.courses if self.courses else None
 
-    def __exit__(self, exc_type, exc_value, traceback):
+    def stop(self):
         self.verboseprint("Closing session...")
         self.session.close()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.stop()
 
 if __name__ == "__main__":
     pass
